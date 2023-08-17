2599b05d9e679a74b86f8fa44a6f1027281b0428
Pallavi Goel <pallavigoel2610@gmail.com>
2023-08-17 16:17:33 +0000

    change player turn according to dice roll

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,23 @@
-console.log("connected!");
+let turn=0;
+let p1sum=0;
+let p2sum=0;
+let num =0;
+
+
+
+document.getElementById("disBtn").addEventListener("click", function () {
+    num = Math.floor(Math.random() * (6 - 1 + 1) + 1)
+    document.getElementById("dice").innerHTML=num
+    
+    if(turn==0)
+        {
+        document.getElementById("turn").innerText= 'Player1';
+        turn = 1;
+        }
+    else
+        {
+            document.getElementById("turn").innerText='Player2';
+            turn=0;
+        }
+    
+})
